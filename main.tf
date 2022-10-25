17c08e2175a42be86b8eebf11a8c42b0adc36e01
Abel Tamrat <atamrat@google.com>
2022-10-25 11:48:45 -0500

    Adding wif

--- main.tf
+++ main.tf
@@ -1,18 +1,13 @@
-     terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "example-organization"
+terraform {
+  backend "gcs" {
+    bucket = "gh-trunk-based-example"
+    prefix = "dev/tfstate"
+  }
+}
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "example-workspace"
-        }
-       }
-     }
-
-     # An example resource that does nothing.
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
